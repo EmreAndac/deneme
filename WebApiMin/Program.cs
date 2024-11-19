beb6edd63437a3a66414771a65abc0b2be81e292
Emre Andac <emre.andac@hotmail.com>
2024-11-19 12:26:11 +0300

    lol

---
+++ WebApiMin/Program.cs
@@ -0,0 +1,58 @@
+using System.Text.Json.Serialization;
+
+namespace WebApiMin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateSlimBuilder(args);
+
+            builder.Services.ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+            });
+
+            var app = builder.Build();
+
+            var sampleTodos = new Todo[] {
+                new(1, "Walk the dog"),
+                new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+                new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+                new(4, "Clean the bathroom"),
+                new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+            };
+
+            var todosApi = app.MapGroup("/todos");
+            todosApi.MapGet("/", () => sampleTodos);
+            todosApi.MapGet("/{id}", (int id) =>
+                sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+                    ? Results.Ok(todo)
+                    : Results.NotFound());
+            //myTodoDelegate myTodoDelegate = new myTodoDelegate(GetGorevler); Daha kolayý var knk
+            todosApi.MapGet("/Gorevler", new Func<Todo[]>(GetGorevler));
+            app.Run();
+        }
+        public delegate Todo[] myTodoDelegate(); 
+        public static Todo[] GetGorevler()
+        {
+            var sampleTodos = new Todo[] {
+                new(1, "Walk the dog"),
+                new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
+                new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
+                new(4, "Clean the bathroom"),
+                new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
+            };
+            return sampleTodos;
+        }
+    }
+
+
+    public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
+
+    [JsonSerializable(typeof(Todo[]))]
+    internal partial class AppJsonSerializerContext : JsonSerializerContext
+    {
+
+    }
+}
